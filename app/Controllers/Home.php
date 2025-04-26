38bfd7d0345af5502ac82fd7772a53b36b4c5756
Cande <candelaxnahir@gmail.com>
2025-04-26 14:49:58 -0300

    fix menu

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,6 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('layouts/headerView') . view('layouts/view') . view('layouts/footerView');
+        return view('layouts/headerView') . view('layouts/vista') . view('layouts/footerView');
     }
 }
