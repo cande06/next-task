dcead7ddb39f6dcec1ed20e55876f40fab9ecbd8
Cande <candelaxnahir@gmail.com>
2025-05-02 12:47:14 -0300

    add login view

---
+++ app/Views/login.php
@@ -0,0 +1,50 @@
+<!-- Content -->
+<nav class="navbar navbar-fixed-top bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+        </a>
+    </div>
+</nav>
+
+<div class="row vh-100">
+
+    <div class="col">
+        <div class="d-flex flex-column align-items-center h-100 justify-content-center">
+            <a class="navbar-brand" href="#">
+                <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+            </a>
+
+            <form class="mb-5">
+                <div class="mb-3">
+                    <label for="loginEmail" class="form-label">Correo</label>
+                    <input type="email" class="form-control" id="loginEmail" aria-describedby="emailHelp">
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
+                <div class="mb-3">
+                    <label for="loginPass" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="loginPass">
+                    <div id="passHelp" class="form-link"><a href="#">¿Olvidaste tu contraseña?</a></div>
+                </div>
+                <!-- <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div> -->
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+
+    <!-- <div class="col-5 d-none d-md-block">
+        <div class="card" style="width: 12rem;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div> -->
+
+</div>
